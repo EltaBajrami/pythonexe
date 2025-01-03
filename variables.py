240f543b1d00a6ae51a0e9bb16ed57befd362c17
unknown <ebajrami@vassar.edu>
2025-01-03 00:23:01 -0500

    Updated variables file

---
+++ variables.py
@@ -0,0 +1,28 @@
+# Vars for numbers in python are defined as follows
+
+x = 5
+type(x)
+
+#Strings declared with single or double quotes
+
+'Hello World' 
+"Hello World"
+
+#If you add two strings together they concatenate
+
+'String 1' + 'String 2'
+'1' + '1' # Will result in "11"
+
+#If you multiply strings you get that string repeated that much time
+
+'1' * 4 # will result in '1111' string
+
+#Cannot add a string and a num different types
+# E.g. '1' + 1 does not work
+
+# BOOLEANS: TRUE OR FALSE Values
+# Use the comparsion operator ==
+# instead of assigned operator =
+
+
+
